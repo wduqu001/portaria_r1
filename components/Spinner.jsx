30c5f7b93845c8dcd7f0e98e8936e333079a2e3b
Willian Duque <willian.almeida@fivevalidation.com>
2022-10-25 15:19:52 -0300

    creates a spinner component

---
+++ components/Spinner.jsx
@@ -0,0 +1,7 @@
+export function Spinner() {
+    return (
+        <div className="text-center p-3">
+            <span className="spinner-border spinner-border-lg align-center"></span>
+        </div>
+    );
+}
